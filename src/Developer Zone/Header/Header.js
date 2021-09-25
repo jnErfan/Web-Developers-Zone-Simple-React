cc68b5282b19733d54ff81e0dbc9c946168798cb
jnErfan <j.n.erfan420@gmail.com>
2021-09-26 01:19:43 +0600

    Assignment 8 Start

---
+++ src/Developer Zone/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Web Developer Zone</h1>
+            <h2>Hire Developers Mounthly Selery : 50000</h2>
+        </div>
+    );
+};
+
+export default Header;
